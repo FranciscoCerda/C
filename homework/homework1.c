7e2e1378736c25925580c7cff7053c625addebc8
FranciscoCerda <78826556+FranciscoCerda@users.noreply.github.com>
2021-02-25 01:50:51 -0600

    homework1.c

--- homework/homework1.c
+++ homework/homework1.c
@@ -1,1 +1,65 @@
+#include <stdio.h>
+
+int main() 
+{
+    int c;
+    int i = 0;
+    char strong[13]={"strong/strong"};
+    char script[13]={"script/script"};
+    printf("\tCode cleaner\n");
+    printf(" Introduce your HTML code below:\n");
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '<')
+        {
+            while ((c = getchar()) == strong[i]) 
+            {
+                ++i;
+            }
+            if (c==script[1]) 
+            {
+                c = getchar();
+                if(c==script[2]) 
+                {
+                    c=getchar();
+                    if (c==script[3]) 
+                    {
+                    while ((c=getchar())!='>') 
+                    {
+                        ;
+                    }
+                    while ((c=getchar())!='>') 
+                    {
+                        ;
+                    }
+                }
+            }
+        }
+        if(i == 13) 
+        {
+            i = 0;
+        }
+        while(c != EOF && c != '>') 
+        {
+            c=getchar();
+        }
+    }
+    else if (i == 6 && i < 13) 
+    {
+            if(c == ' ') 
+            {
+                printf("_");
+                c = getchar();
+            }
+        putchar(c);
+        }
+        if ((i != 6 && c != '<' && c != '>') || c == '\n') 
+        {
+            putchar (c);
+            i = 0;
+        }
+    }
+}
+
 
